d11ad906251efdb3be52ff77ae30430ea701b8d0
Praveen Kommareddy <praveenkommareddy@Praveens-MacBook-Pro.local>
2017-12-09 10:31:18 -0500

    Formatting and minor changes

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,28 +3,25 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_instance" {
+  ami             = "${lookup(var.ami_id, var.aws_region)}"
+  instance_type   = "${var.instance_type}"
+  subnet_id       = "${var.subnet_id}"
+  security_groups = ["${aws_security_group.default.id}"]
+  user_data       = "${file("userdata.sh")}"
+  key_name        = "${var.key_name}"
 
-ami      = "${lookup(var.ami_id, var.aws_region)}"
-instance_type = "${var.instance_type}"
-subnet_id = "${var.subnet_id}"
-
-# Security group
-security_groups = ["${aws_security_group.default.id}"]
-user_data       = "${file("userdata.sh")}"
-key_name        = "${var.key_name}"
-
-tags {
-  Name          = "${var.ec2_name}"
-  Owner         = "${var.ec2_owner}"
-}
-
+  tags {
+    Name  = "${var.ec2_name}"
+    Owner = "${var.ec2_owner}"
+  }
 }
 
 # Default security group to access
 # the instances over SSH and HTTP
+# SG will be deleted with tf destroy
 resource "aws_security_group" "default" {
   name        = "terraform_example_sg"
-  description = "Used in the terraform"
+  description = "Used in terraform deployments"
 
   # SSH access from anywhere
   ingress {
@@ -48,5 +45,9 @@ resource "aws_security_group" "default" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "terraform_example_sg"
   }
 }
